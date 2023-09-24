d23a299e54331d166459bed3e796c40a99c748ab
Ronny Sandino Alvarado <rosandinoal@est.utn.ac.cr>
2023-09-24 16:24:13 -0600

    Modificacion en TransferDto
    modificaciones

--- src/Transacciones/Dtos/TransferDto.java
+++ src/Transacciones/Dtos/TransferDto.java
@@ -12,20 +12,18 @@ import java.util.Date;
  *
  * @author rsand
  */
-public class TransferDto extends Transaction {
+public class TransferDto {
     private Account destination;
+    private double amount;
+    private Account source;
 
     public TransferDto(Account destination, double amount, Account source) {
-        super(amount, source);
         this.destination = destination;
+        this.amount = amount;
+        this.source = source;
     }
     
     public Account getDestination() {
         return destination;
-    }
-    
-    @Override
-    public boolean execute(){
-        return true;
     }
 }
