01f8217422fbe724282907484c535fce2dad3f42
Denzel Herrera García <denzelhgarcia24680@gmail.com>
2023-10-09 02:50:47 -0600

    Singleton y DAO Controller
    Se implemento el patrón de diseño Singleton a las clases CreditCardDaoList, UserDaoList y WithdrawalDaoList, y se crearon el Controller para las clases DollarAccountDao, CustomerDao y TransferDao

---
+++ src/Transacciones/Daos/TransferDaoController.java
@@ -0,0 +1,36 @@
+package Transacciones.Daos;
+
+import Controller.Controller;
+import Views.View;
+import java.util.List;
+
+public class TransferDaoController implements Controller<TransferDaoList>{
+    private View view;
+    private TransferDaoList list;
+
+    @Override
+    public boolean create(TransferDaoList obj) {
+        return false;
+    }
+
+    @Override
+    public TransferDaoList read(String id) {
+        return null;
+    }
+
+    @Override
+    public List<TransferDaoList> readAll() {
+        return null;
+    }
+
+    @Override
+    public boolean update(TransferDaoList obj) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(TransferDaoList obj) {
+        return false;
+    }
+    
+}
--- src/Transacciones/Daos/WithdrawalDaoList.java
+++ src/Transacciones/Daos/WithdrawalDaoList.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Transacciones.Daos;
 
 import Dao.Dao;
@@ -10,40 +6,45 @@ import Transacciones.Dtos.WithdrawalDto;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- *
- * @author rsand
- */
 public class WithdrawalDaoList implements Dao<WithdrawalDto>{
+    
      private HashMap<String,UserDto> withdrawalList;
+     private static WithdrawalDaoList instance;
 
-    public WithdrawalDaoList() {
+    private WithdrawalDaoList() {
         withdrawalList = new HashMap();
+    }
+    
+    public static WithdrawalDaoList getInstance() {
+        if (instance == null) {
+            instance = new WithdrawalDaoList();
+        }
+        return instance;
     }
 
     @Override
     public boolean create(WithdrawalDto obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         return false;
     }
 
     @Override
     public WithdrawalDto read(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         return null;
     }
 
     @Override
     public List<WithdrawalDto> readAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         return null;
     }
 
     @Override
     public boolean update(WithdrawalDto obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         return false;
     }
 
     @Override
     public boolean delete(WithdrawalDto obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         return false;
     }
 
 }
