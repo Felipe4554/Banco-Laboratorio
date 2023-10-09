57e2c0fcf2c6eacb62e6fb3973fe85e86acd866b
Felipe Chacon Vargas <felichaconv@gmail.com>
2023-10-09 12:50:45 -0600

    AccountSinpe
    Cuenta de sinpe

--- src/Account/AccountSinpe.java
+++ src/Account/AccountSinpe.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Bank;
+package Account;
 
 /**
  *
  * @author ´Felipe Chacón
  */
-public class Bank {
+public class AccountSinpe {
     
 }
